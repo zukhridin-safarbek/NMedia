7a742437cd7e0fd13267000ffd0e770b636980ff
prince-tagaev <asaba@internet.ru>
2022-07-14 23:13:16 +0300

    add function count likes and create design post

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,45 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import kotlin.math.floor
+import kotlin.math.log10
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+        lateinit var binding: ActivityMainBinding
+        var  likesCount = 2299
+        var forDecLikesCount = likesCount
+        var pressedTime = 0L
+        lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if (likesCount == 0){
+            binding.postLikesCount.text = ""
+        }else binding.postLikesCount.text = "${PrettyCount().prettyCount(likesCount)}"
+        binding.likeIcon.setOnClickListener {
+               if (pressedTime + 500 > System.currentTimeMillis()){
+                   likesCount++
+                   forDecLikesCount++
+                   handler = Handler(mainLooper)
+                   handler.postDelayed({
+                       binding.postLikesCount.text = "${PrettyCount().prettyCount(likesCount)}"
+
+                   },1000)
+                   binding.postLikesCount.text = "${forDecLikesCount}"
+
+               }
+            pressedTime = System.currentTimeMillis()
+               }
+
+
     }
+
+
 }
