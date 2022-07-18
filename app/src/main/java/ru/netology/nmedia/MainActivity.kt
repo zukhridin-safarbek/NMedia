5cfe733569c36e8e5c547ef1a4f41129dc1485c2
Zukhridin Safarbek uulu <104100827+zukh21@users.noreply.github.com>
2022-07-19 00:21:04 +0300

    Update MainActivity.kt

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -20,25 +20,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        if (likesCount == 0){
-            binding.postLikesCount.text = ""
-        }else binding.postLikesCount.text = "${PrettyCount().prettyCount(likesCount)}"
-        binding.likeIcon.setOnClickListener {
-               if (pressedTime + 500 > System.currentTimeMillis()){
-                   likesCount++
-                   forDecLikesCount++
-                   handler = Handler(mainLooper)
-                   handler.postDelayed({
-                       binding.postLikesCount.text = "${PrettyCount().prettyCount(likesCount)}"
-
-                   },1000)
-                   binding.postLikesCount.text = "${forDecLikesCount}"
-
-               }
-            pressedTime = System.currentTimeMillis()
-               }
-
-
     }
 
 
