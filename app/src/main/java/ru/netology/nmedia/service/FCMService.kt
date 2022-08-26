a09a69f879c337f0289bf524b5b6edca26c0cea2
prince-tagaev <asaba@internet.ru>
2022-08-26 21:09:59 +0300

    deleted fcm.json

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -1,19 +1,21 @@
 package ru.netology.nmedia.service
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import com.google.firebase.messaging.ktx.remoteMessage
 import com.google.gson.Gson
 import ru.netology.nmedia.R
 import ru.netology.nmedia.channelId
-import ru.netology.nmedia.dto.Action
 import ru.netology.nmedia.dto.Like
 import ru.netology.nmedia.dto.Post
-import ru.netology.nmedia.exception.MyIllegalArgumentException
+import java.io.InputStream
 import java.lang.Exception
+import java.net.HttpURLConnection
+import java.net.URL
 import kotlin.random.Random
 
 class FCMService : FirebaseMessagingService() {
@@ -22,38 +24,23 @@ class FCMService : FirebaseMessagingService() {
     private val gson = Gson()
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(action, "From: ${remoteMessage.from}")
-
-        // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(action, "Message data payload: ${remoteMessage.data}")
-
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                sendNotification(gson.fromJson(remoteMessage.data[content], Post::class.java))
-            }
+            sendNotification(gson.fromJson(remoteMessage.data[content], Post::class.java))
         }
-
-        // Check if message contains a notification payload.
-        remoteMessage.notification?.let {
-            Log.d(action, "Message Notification Body: ${it.body}")
-            Log.d(action, "Message Notification Body: ${it.title}")
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
 
     private fun sendNotification(post: Post) {
         val notification = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(post.author)
-            .setContentText(post.content)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setStyle(NotificationCompat.BigTextStyle()
+                .bigText(post.content))
+            .setContentText(post.publishedDate)
+            .setLargeIcon(post.videoLink?.let { getLargeImageFromUrl(it) })
+            .setPriority(NotificationCompat.PRIORITY_MAX)
             .build()
-
         NotificationManagerCompat.from(this)
             .notify(Random.nextInt(100_000), notification)
     }
@@ -61,14 +48,15 @@ class FCMService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         println(token)
     }
-    private fun handleLike(content: Like){
+
+    private fun handleLike(content: Like) {
         val notification = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(
                 getString(
-                        R.string.notification_user_liked,
-                        content.userName,
-                        content.postAuthor,
+                    R.string.notification_user_liked,
+                    content.userName,
+                    content.postAuthor,
                 )
             )
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
@@ -76,5 +64,19 @@ class FCMService : FirebaseMessagingService() {
 
         NotificationManagerCompat.from(this)
             .notify(Random.nextInt(100_000), notification)
+    }
+
+    private fun getLargeImageFromUrl(imgUrl: String): Bitmap? {
+        return try {
+            val url = URL(imgUrl)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.connect()
+
+            val input: InputStream = connection.inputStream
+            BitmapFactory.decodeStream(input)
+        }catch (e: Exception){
+            e.printStackTrace()
+            null
+        }
     }
 }
